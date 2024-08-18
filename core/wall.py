9b8e29745a64d973a00273528f671b1d7e1d0032
John Orchard <jtorchard@protonmail.com>
2024-08-18 14:34:32 +0100

    Add code to initialise the wall

--- core/wall.py
+++ core/wall.py
@@ -1,4 +1,5 @@
 from itertools import chain
+from random import shuffle
 
 from .tile import Tile
 from .data import (
@@ -14,10 +15,18 @@ from .data import (
 
 class Wall:
     def __init__(self):
+        self.tiles = []
+        self.initialise_wall()
+
+    def initialise_wall(self):
         self.tiles = [
             Tile(tile) for tile in chain(
                 characters, bamboos, circles, dragons, winds, flowers, seasons)
         ] * 4
+        self._shuffle_wall()
+
+    def _shuffle_wall(self):
+        shuffle(self.tiles)
 
     def __str__(self):
         return "".join([tile.utf8 for tile in self.tiles])
