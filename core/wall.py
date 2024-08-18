52c3340b9a07cdaedf45dc4b3cfd2192ad01ee5a
John Orchard <jtorchard@protonmail.com>
2024-08-18 14:37:55 +0100

    Refactor to support dead wall

--- core/wall.py
+++ core/wall.py
@@ -15,18 +15,19 @@ from .data import (
 
 class Wall:
     def __init__(self):
-        self.tiles = []
+        self.alive_tiles = []
+        self.dead_tiles = []
         self.initialise_wall()
 
     def initialise_wall(self):
-        self.tiles = [
+        self.alive_tiles = [
             Tile(tile) for tile in chain(
                 characters, bamboos, circles, dragons, winds, flowers, seasons)
         ] * 4
         self._shuffle_wall()
 
     def _shuffle_wall(self):
-        shuffle(self.tiles)
+        shuffle(self.alive_tiles)
 
     def __str__(self):
         return "".join([tile.utf8 for tile in self.tiles])
