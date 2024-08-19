53f66c4b6d8b5dacbffe274f506eb04198acbb8c
John Orchard <jtorchard@protonmail.com>
2024-08-19 16:11:52 +0100

    Add flag for checking if wall is shuffled

--- core/wall.py
+++ core/wall.py
@@ -17,18 +17,22 @@ class Wall:
     def __init__(self, shuffle_wall=True):
         self.alive_tiles = []
         self.dead_tiles = []
+        self.is_shuffled = False
         self.initialise_wall()
         if shuffle_wall:
-            self._shuffle_wall()
+            self.shuffle_wall()
 
     def initialise_wall(self):
         self.alive_tiles = [
             Tile(tile) for tile in chain(
                 characters, bamboos, circles, dragons, winds, flowers, seasons)
         ] * 4
+        self.dead_tiles = []
+        self.is_shuffled = False
 
-    def _shuffle_wall(self):
+    def shuffle_wall(self):
         shuffle(self.alive_tiles)
+        self.is_shuffled = True
 
     def take_live_wall(self):
         return self.alive_tiles.pop()
@@ -41,3 +45,6 @@ class Wall:
 
     def __str__(self):
         return "".join([tile.utf8 for tile in chain(self.alive_tiles, self.dead_tiles)])
+
+    def __len__(self):
+        return len(self.alive_tiles + self.dead_tiles)
