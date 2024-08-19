441c760bbee5702f74b1123af1ee64e790f82c18
John Orchard <jtorchard@protonmail.com>
2024-08-19 18:01:53 +0100

    Add tests to check for shuffle

--- tests/test_wall.py
+++ tests/test_wall.py
@@ -3,3 +3,11 @@ from core.wall import Wall
 
 def test_wall_has_correct_number_of_tiles(wall=Wall()):
     assert len(wall) == 168
+
+
+def test_wall_is_shuffled(wall=Wall()):
+    assert wall.is_shuffled is True
+
+
+def test_wall_is_not_shuffled(wall=Wall(shuffle_wall=False)):
+    assert wall.is_shuffled is False
