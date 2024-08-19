9ce158702659cf8f628d093441af426df0847c59
John Orchard <jtorchard@protonmail.com>
2024-08-19 16:01:45 +0100

    Make shuffling optional when creating a wall

--- core/wall.py
+++ core/wall.py
@@ -14,17 +14,18 @@ from .data import (
 
 
 class Wall:
-    def __init__(self):
+    def __init__(self, shuffle_wall=True):
         self.alive_tiles = []
         self.dead_tiles = []
         self.initialise_wall()
+        if shuffle_wall:
+            self._shuffle_wall()
 
     def initialise_wall(self):
         self.alive_tiles = [
             Tile(tile) for tile in chain(
                 characters, bamboos, circles, dragons, winds, flowers, seasons)
         ] * 4
-        self._shuffle_wall()
 
     def _shuffle_wall(self):
         shuffle(self.alive_tiles)
