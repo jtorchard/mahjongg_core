35658e0d47150ad6e223b9efb1e715bc2cbdf006
John Orchard <jtorchard@protonmail.com>
2024-08-19 16:12:06 +0100

    Add test for creating wall

--- tests/test_wall.py
+++ tests/test_wall.py
@@ -1,13 +1,5 @@
 from core.wall import Wall
 
-def test_east_wind_data_correct(tile=Tile(east_wind)):
-    assert tile.utf8 == "🀀"
-    assert tile.name == "east_wind"
-    assert tile.rank == "east"
-    assert tile.suit == "wind"
-    assert tile.is_dragon is False
-    assert tile.is_wind is True
-    assert tile.is_special is False
-    assert tile.is_honour is True
-    assert tile.is_suit is False
-    assert tile.is_terminal is False
+
+def test_wall_has_correct_number_of_tiles(wall=Wall()):
+    assert len(wall) == 168
