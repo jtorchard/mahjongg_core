73da61fe58de5cc6ac308950be654ce0095ced8e
John Orchard <jtorchard@protonmail.com>
2024-08-21 12:32:02 +0100

    Add stub for game module

---
+++ core/game.py
@@ -0,0 +1,18 @@
+from core.wall import Wall
+
+
+class Game:
+
+    def __init__(self):
+        self.players = None
+        self.wind = None
+        self.round = None
+        self.hand = None
+        self.seats = None
+        self.wall = Wall()
+
+    def start(self):
+        pass
+
+    def deal(self):
+        pass
