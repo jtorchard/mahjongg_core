77cc9ad334ea345727829bb7d2313bc983a85b4e
John Orchard <jtorchard@protonmail.com>
2024-08-21 15:48:28 +0100

    Add basic player init and wall shuffle for game

--- core/game.py
+++ core/game.py
@@ -1,15 +1,16 @@
+from core.player import Player
 from core.wall import Wall
 
 
 class Game:
 
-    def __init__(self):
-        self.players = None
-        self.wind = None
+    def __init__(self, shuffle_wall=True):
+        self.players = [Player(number) for number in range(1, 5)]
+        self.wind = "East"
         self.round = None
-        self.hand = None
         self.seats = None
-        self.wall = Wall()
+        self.turn = None
+        self.wall = Wall(shuffle_wall=shuffle_wall)
 
     def start(self):
         pass
