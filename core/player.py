61a08d892e2bc169dc14ec72488ce8be762adbd4
John Orchard <jtorchard@protonmail.com>
2024-08-22 17:45:06 +0100

    Add getters for player

--- core/player.py
+++ core/player.py
@@ -6,7 +6,18 @@ class Player:
         self.player_id = player_id
         self.score = 2000
         self.hand = []
-        self.wind = None
 
     def __str__(self):
         return f"player_{self.player_id}"
+
+    def add_tile(self, tile):
+        self.hand.append(tile)
+
+    def get_hand(self):
+        return self.hand
+
+    def get_score(self):
+        return self.score
+
+    def get_id(self):
+        return self.player_id
