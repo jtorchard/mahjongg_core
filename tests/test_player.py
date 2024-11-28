549773d33e247fa694fa7115fb8a5254a2b164b5
John Orchard <jtorchard@protonmail.com>
2024-11-28 15:31:56 +0000

    Parameterise player test

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,11 +5,16 @@ from core.tile import Tile
 from core.data import east_wind
 
 
-def test_player_has_correct_defaults(player=Player(1)):
-    assert player.get_id() == 1
+@pytest.mark.parametrize("number, name", [(1, "player_1"),
+                                          (2, "player_2"),
+                                          (3, "player_3"),
+                                          (4, "player_4")])
+def test_player_has_correct_defaults(number, name):
+    player = Player(number)
+    assert player.get_id() == number
     assert player.get_score() == 2000
     assert player.get_hand() == []
-    assert str(player) == "player_1"
+    assert str(player) == name
 
 
 def test_hand_has_tile_after_adding(player=Player(1)):
