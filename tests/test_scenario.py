638e9d37d51fae9a14237f69210918358d608224
John Orchard <jtorchard@protonmail.com>
2024-12-14 17:37:58 +0000

    Add test for general game play-through

---
+++ tests/test_scenario.py
@@ -0,0 +1,21 @@
+"""
+    Tests general play-through scenarios.
+"""
+
+import pytest
+
+from core.big_state import Game
+
+
+@pytest.fixture
+def game_random_seed():
+    return Game(seed=None)
+
+
+@pytest.fixture
+def game_fixed_seed():
+    return Game(seed=69)
+
+
+def test_game_scenario(game_fixed_seed):
+    game_fixed_seed.deal()
