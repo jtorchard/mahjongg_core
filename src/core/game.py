9fede3fe6b3d8ec3bb255a2dd453f77552948a1d
John Orchard <jtorchard@protonmail.com>
2024-12-14 21:42:15 +0000

    Add more logging at INFO level

--- src/core/game.py
+++ src/core/game.py
@@ -18,7 +18,8 @@ from src.models.tile import (
 )
 from src.models.wind import Wind
 
-logger.add("mahjong.log", colorize=True)
+logger.remove()  # Turn off default console logger
+logger.add("mahjong.log", colorize=True, level="INFO")
 
 NUM_OF_TILES_SUIT: int = 4
 NUM_OF_TILES_HONOUR: int = 4
@@ -27,7 +28,7 @@ SIZE_OF_DEAD_WALL: int = 16
 
 class Game:
     def __init__(self, seed: Optional[int] = None):
-        logger.debug(f"Initialising game with seed: {seed}")
+        logger.info(f"Initialising game with seed: {seed}")
         self.seed: Optional[int] = seed
         self.hand: int = 1
         self.seating_counter: int = 1
@@ -54,7 +55,7 @@ class Game:
         return {p.seat: p for p in self.players}[wind]
 
     def build_wall(self) -> None:
-        logger.debug("Building wall...")
+        logger.info("Building wall...")
         self.live_wall = [
             tile()
             for tile in chain(
@@ -67,23 +68,34 @@ class Game:
                 seasons,
             )
         ]
-        logger.debug(f"Wall built with {len(self.live_wall)} tiles")
+        logger.info(f"Wall built with {len(self.live_wall)} tiles")
 
     def break_wall(self) -> None:
+        logger.info("Breaking wall...")
         self.dead_wall = self.live_wall[:SIZE_OF_DEAD_WALL]
         self.live_wall = self.live_wall[SIZE_OF_DEAD_WALL:]
         self.loose_tiles = [self.dead_wall.pop(), self.dead_wall.pop()]
+        logger.info("Wall broken")
 
     def shuffle_seats(self) -> None:
+        logger.info("Shuffling seats...")
         _winds = list(Wind)
         shuffle(_winds)
         self.player_1.seat, self.player_2.seat, self.player_3.seat, self.player_4.seat, = _winds
+        logger.info("Seats shuffled")
 
     def change_seats(self) -> None:
+        player_seats = {p.number: p.seat for p in self.players}
+        logger.info(f"Changing seats...")
+
         for player in self.players:
             player.seat = player.seat.next()
 
+        for p in self.players:
+            logger.info(f"Seat changed for Player {p.number}: {player_seats[p.number]} -> {p.seat}")
+
     def deal(self) -> None:
+        logger.info(f"Dealing tiles...")
         # Take twelve tiles each
         players_by_wind: List[Player] = sorted(self.players, key=lambda p: p.seat)
         for _ in range(3):
@@ -97,3 +109,4 @@ class Game:
 
         # East takes a fourteenth tile
         self.player_by_wind(Wind.EAST).hand.append(self.live_wall.pop())
+        logger.info(f"Tiles dealt")
