d768bf2431d956b5544ec7a0dbbc571df4f99fc4
John Orchard <jtorchard@protonmail.com>
2024-12-14 19:01:52 +0000

    Add name to player with faker support

--- src/models/player.py
+++ src/models/player.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from faker import Faker
 from pydantic import BaseModel, Field
 
 from .tile import Tile
@@ -9,6 +10,7 @@ from .wind import Wind
 # noinspection PyDataclass
 class Player(BaseModel):
     seat: Wind
+    name: str = Field(default_factory=lambda: f"{Faker().word()} {Faker().word()}")
     number: int = Field(gt=0, lt=5)
     hand: List[Tile] = Field(default_factory=list)
     score: int = 2000
