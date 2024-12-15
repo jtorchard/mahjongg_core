38743181f6f1cd832dafa81829c92f48f33f768b
John Orchard <jtorchard@protonmail.com>
2024-12-15 17:54:19 +0000

    Add new functionality for hand class
    Adds support for analysing the hand implicitly when changes are made.

--- src/core/hand.py
+++ src/core/hand.py
@@ -2,23 +2,71 @@
     Handles the tiles in a player's hand.
     Ordering, sorting, determining special cases etc.
 """
-from typing import List
+from collections import Counter, UserList
+from functools import wraps
+from typing import Callable
 
-from src.models.tile import Tile
+from loguru import logger
+
+logger.remove()  # Turn off default console logger
+logger.add("mahjong.log", colorize=True, level="INFO")
 
 
-class Hand:
-    def __init__(self) -> None:
-        self._tiles: List[Tile] = []
+def hand_mutated(f: Callable) -> Callable:
+    @wraps(f)
+    def inner(self, *args, **kwargs):
+        f(self, *args, **kwargs)
+        self.analyse_hand(*args, **kwargs)
 
-    def __getitem__(self, index: int) -> Tile:
-        return self._tiles[index]
+    return inner
 
-    def __len__(self) -> int:
-        return len(self._tiles)
+
+class Hand(UserList):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.one_chance_hand = False
+        self.can_go_out = False
+        self.wind_of_the_round = False
+        self.chows = 0
+        self.concealed_pungs = 0
+        self.concealed_kongs = 0
+        self.exposed_pungs = 0
+        self.exposed_kongs = 0
+        self.pairs = 0
+        self.seasons = 0
+        self.flowers = 0
+
+    def __setitem__(self, i, item):
+        self.data[i] = item
+
+    def __delitem__(self, i):
+        del self.data[i]
+
+    @hand_mutated
+    def append(self, item):
+        self.data.append(item)
+
+    @hand_mutated
+    def remove(self, i):
+        self.data.remove(i)
+
+    @hand_mutated
+    def pop(self, i=-1):
+        self.data.pop(i)
+
+    @hand_mutated
+    def insert(self, i, item):
+        self.data.insert(i, item)
+
+    @hand_mutated
+    def extend(self, other):
+        self.data.extend(other)
 
     def __str__(self) -> str:
-        return " ".join([t.utf8 for t in self._tiles])
+        return " ".join([t.utf8 for t in self.data])
 
-    def tiles(self):
-        return self._tiles
+    def analyse_hand(self, *args, **kwargs):
+        logger.info(f"Hand changed. args: {args} kwargs: {kwargs} - Analysing hand...")
+        c = Counter(self.data)
+        logger.info(f"Potential Pairs: {list(filter(lambda a: a[1] >= 2, c.items()))}")
+        logger.info(f"Tiles: |{'| |'.join([f'{k.name}:{v}' for k, v in c.items()])}|")
