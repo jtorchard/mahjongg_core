192cd538d0f7e5ae03eacb29a6a398272fdba324
John Orchard <jtorchard@protonmail.com>
2024-12-15 15:01:25 +0000

    Update hand.py
    Now a custom type to handle tiles in a player's hand.

--- src/core/hand.py
+++ src/core/hand.py
@@ -1,5 +1,6 @@
 """
-Contains data for each hand played.
+    Handles the tiles in a player's hand.
+    Ordering, sorting, determining special cases etc.
 """
 from typing import List
 
@@ -8,13 +9,16 @@ from src.models.tile import Tile
 
 class Hand:
     def __init__(self) -> None:
-        self.tiles: List[Tile] = []
+        self._tiles: List[Tile] = []
 
     def __getitem__(self, index: int) -> Tile:
-        return self.tiles[index]
+        return self._tiles[index]
 
     def __len__(self) -> int:
-        return len(self.tiles)
+        return len(self._tiles)
 
     def __str__(self) -> str:
-        return " ".join([t.utf8 for t in self.tiles])
+        return " ".join([t.utf8 for t in self._tiles])
+
+    def tiles(self):
+        return self._tiles
