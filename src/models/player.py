e2992fef2eaf8e89e54cb508625d13c4c748c411
John Orchard <jtorchard@protonmail.com>
2024-12-18 09:48:10 +0000

    Capitalise player names

--- src/models/player.py
+++ src/models/player.py
@@ -6,7 +6,17 @@ from .wind import Wind
 
 class Player(BaseModel):
     seat: Wind
-    name: str = Field(default_factory=lambda: f"{Faker().word()} {Faker().word()}")
+    name: str = Field(default_factory=lambda: f"{Faker().word().title()} {Faker().word().title()}")
     number: int = Field(gt=0, lt=5)
     hand: list = Field(default_factory=list)
     score: int = 2000
+
+#
+# class Player:
+#
+#     def __init__(self):
+#         self.seat = None
+#         self.name = ""
+#         self.number = 1
+#         self.hand = []
+#         self.score = 2000
